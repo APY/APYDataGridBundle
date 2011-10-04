f839114ac0ae7f0f0221b160c72da815f137d7f8
Abhoryo <zenyaku@free.fr>
2011-10-04 03:30:20 +0200

    Add Boolean column with auto values, see #21

---
+++ Grid/Column/Boolean.php
@@ -0,0 +1,21 @@
+<?php
+
+    /*
+ * This file is part of the DataGridBundle.
+ *
+ * (c) Stanislav Turza <sorien@mail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sorien\DataGridBundle\Grid\Column;
+
+class Boolean extends Select
+{
+    public function __initialize(array $params)
+    {
+        $params['values'] = array('1'=>'true','0'=>'false');
+        parent::__initialize($params);  
+    }
+}
