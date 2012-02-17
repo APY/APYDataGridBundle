929034c63c4e5e5e1de54829eab0fcb2b63714e3
Petit Yoann <zenyaku@free.fr>
2012-02-17 22:18:20 +0800

    Allow whitespaces in Source/Columns order definition. @GRID\Source(columns="id, username, email")

--- Grid/Mapping/Source.php
+++ Grid/Mapping/Source.php
@@ -21,7 +21,7 @@ class Source
 
     public function __construct($metadata = array())
     {
-        $this->columns = isset($metadata['columns']) ? explode(',', $metadata['columns']) : array();
+        $this->columns = isset($metadata['columns']) ? array_map('trim', explode(',', $metadata['columns'])) : array();
         $this->filterable = !(isset($metadata['filterable']) && $metadata['filterable']);
     }
 
