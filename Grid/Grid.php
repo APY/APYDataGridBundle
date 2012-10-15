67272233a3846e7d71e259dcc3db606354300cb8
Pascal <michelpa@gmail.com>
2012-10-15 19:35:17 +0200

    ajax reload

--- Grid/Grid.php
+++ Grid/Grid.php
@@ -1555,6 +1555,17 @@ class Grid
      */
     public function getGridResponse($param1 = null, $param2 = null, Response $response = null)
     {
+        
+        $this->createHash();
+        $this->requestData = (array) $this->request->get($this->hash);
+        
+        if ($this->request->isXmlHttpRequest() && array_key_exists('ajax', $this->requestData) &&  $this->requestData['ajax']) {
+            //ajax call
+            $this->ajax_call = true;
+        } else {
+            $this->ajax_call = false;
+        }
+        
         if ($this->isReadyForExport()) {
             return $this->getExportResponse();
         }
@@ -1573,6 +1584,15 @@ class Grid
             $parameters = array_merge(array('grid' => $this), $parameters);
             
             if ($view === null) {
+                if ($this->ajax_call) {
+                    $resp = new Response();
+                    $parameters['withjs'] = false;
+                    $content = $this->container->get('twig')->loadTemplate($this->getTemplate())->renderBlock('grid', $parameters);
+                    $resp->setContent($content);
+
+                    return $resp;
+                }
+                
                 return $parameters;
             } else {
                 return $this->container->get('templating')->renderResponse($view, $parameters, $response);
