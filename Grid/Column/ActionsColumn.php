17923f79dc5bf2079cdaf11e68cb5bf426fb3609
Pierre du Plessis <pierre@pcservice.co.za>
2012-10-30 14:31:36 +0200

    Allow row actions to manipulate data in the manipulateRender function without overriding the defaults

--- Grid/Column/ActionsColumn.php
+++ Grid/Column/ActionsColumn.php
@@ -112,7 +112,8 @@ class ActionsColumn extends Column
     public function getActionsToRender($row)
     {
         $list = $this->rowActions;
-        foreach($list as $i=>$action) {
+        foreach($list as $i=>$a) {
+        	$action = clone $a;
             $list[$i] = $action->render($row);
             if(null === $list[$i]) {
                 unset($list[$i]);
