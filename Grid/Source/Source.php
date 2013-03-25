ef71ac56cd859ed847da7946b3fbbf1d2fe6eb1b
Flofr <f.carlier@mb-line.com>
2013-03-25 15:00:00 +0100

    BugFix search from date type field

--- Grid/Source/Source.php
+++ Grid/Source/Source.php
@@ -266,7 +266,7 @@ abstract class Source implements DriverInterface
                         $value = $filter->getValue();
 
                         // Normalize value
-                        if (!$dataIsNumeric && !$value instanceof \DateTime) {
+                        if (!$dataIsNumeric && !($value instanceof \DateTime)) {
                             $value = $this->prepareStringForLikeCompare($value);
                             switch ($operator) {
                                 case Column\Column::OPERATOR_EQ:
