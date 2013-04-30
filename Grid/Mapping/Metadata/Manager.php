46d42fdbc7ec63296d2255c94ae480e64009edb6
Matt Daum <matt@setfive.com>
2013-04-30 08:54:40 -0400

    Updated the manager to use only one type of each driver per run, as they are inserted in -1 order the most recent one will be on top

--- Grid/Mapping/Metadata/Manager.php
+++ Grid/Mapping/Metadata/Manager.php
@@ -44,9 +44,15 @@ class Manager
     {
         $metadata = new Metadata();
 
-        $columns = $fieldsMetadata = $groupBy = array();
+        $columns = $fieldsMetadata = $groupBy = $usedClassDrivers = array();
 
         foreach ($this->getDrivers() as $driver) {
+
+            if(in_array(get_class($driver),$usedClassDrivers))
+              continue;
+
+            $usedClassDrivers[]=get_class($driver);
+
             $columns = array_merge($columns, $driver->getClassColumns($className, $group));
             $fieldsMetadata[] = $driver->getFieldsMetadata($className, $group);
             $groupBy = array_merge($groupBy, $driver->getGroupBy($className, $group));
