75998d853f1ec672f53afb5b69949299595c2e71
Nicolas Potier <nicolas.potier@acseo-conseil.fr>
2013-08-08 15:09:54 +0200

    WORK IN PROGESS : emplacement des fichiers YML dans @MyBundle/Resources/config/grid/MyEntity.MyGroup.yml

--- Grid/Mapping/Driver/Yaml.php
+++ Grid/Mapping/Driver/Yaml.php
@@ -27,12 +27,19 @@ class Yaml implements DriverInterface
     protected $loaded;
     protected $groupBy;
 
+    protected $kernel;
 	/**
      * File extension
      * @var string
      */
     protected $_extension = '.grid.yml';
 
+
+    public function __construct($kernel)
+    {
+        $this->kernel = $kernel;
+    }
+    
     public function getClassColumns($class, $group = 'default')
     {
 		$this->loadMetadataFromReader($class, $group);
@@ -57,7 +64,7 @@ class Yaml implements DriverInterface
         if (isset($this->loaded[$className][$group])) return;
 
     	$instance = new \ReflectionClass($className);
-        $content = $this->getYamlContent($instance);
+        $content = $this->getYamlContent($instance, $group);
 
         // TODO valider la présence de ces clés
         // Peut être en passant par un TreeBuilder ?
@@ -68,35 +75,18 @@ class Yaml implements DriverInterface
 
         $columns = $content[$class]["Source"]["columns"];
         foreach ($columns as $colName => $properties) {
-        	if (isset($properties["group"])) {
-        		$groups = explode(",", $properties["group"]);
-        	}
-        	else {
-        		$groups = array("default");
-        	}
-        	
-        	foreach ($groups as $group) {
-        		if (!isset($this->columns[$class][$group])) {
-        			$this->columns[$class][$group] = array();
-        		}
-        		$this->columns[$class][$group][] = $colName;
-        	}
+    		if (!isset($this->columns[$class][$group])) {
+    			$this->columns[$class][$group] = array();
+    		}
+    		$this->columns[$class][$group][] = $colName;
         }
 
         $fields = $content[$class]["Columns"];
         foreach ($fields as $fieldName => $properties) {
-           if (isset($properties["group"])) {
-                $groups = explode(",", $properties["group"]);
+            if (!isset($this->fields[$class][$group])) {
+                $this->fields[$class][$group] = array();
             }
-            else {
-                $groups = array("default");
-            }
-            foreach ($groups as $group) {
-                if (!isset($this->fields[$class][$group])) {
-                    $this->fields[$class][$group] = array();
-                }
-                $this->fields[$class][$group][$fieldName] = $properties;
-            }
+            $this->fields[$class][$group][$fieldName] = $properties;
         }
 
         $groupBys = isset($content[$class]["Source"]["groupBy"])? $content[$class]["Source"]["groupBy"] : array();
@@ -109,25 +99,66 @@ class Yaml implements DriverInterface
             $this->filterable[$class][$group] = $filterable;
         }
 
-
-
         $this->loaded[$className][$group] = true;
     }
 
-    private function getYamlContent($instance)
+    /**
+     * Get The Yaml file associated to a class and a group
+     * @param \ReflectionClass $instance an instance of the class
+     * @param String $group the name of the group
+     * @return array the parsed YAML file
+     */
+    private function getYamlContent($instance, $group)
     {   	
-    	$yamlFile = str_replace(".php", $this->_extension, $instance->getFileName());
-    	if (!file_exists($yamlFile)) {
-    		throw new \Exception("Yaml configuration file was not found for $class");
-    	}
+        $yamlFile = $this->locateYamlFile($instance, $group);
 		
 		$yamlParser = new Parser();
-		try {
-		    $content = $yamlParser->parse(file_get_contents($yamlFile));
-		} catch (ParseException $e) {
-		    printf("Unable to parse the YAML string: %s", $e->getMessage());
-		}	
+	    $content = $yamlParser->parse(file_get_contents($yamlFile));
 
 		return $content;
+    }
+
+    /**
+     * Locate a YAML File based on a directory convention
+     * @param \ReflectionClass $instance an instance of the class
+     * @param String $group the name of the group
+     * @return String the file name
+     */
+    private function locateYamlFile($instance, $group) {
+        $bundleName = $this->getBundleNameForClass($instance->getName());
+
+        $fileToLocate = sprintf("@%s/Resources/config/grid/%s.%s%s", 
+            $bundleName,
+            basename($instance->getFileName(), ".php"),
+            $group,
+            $this->_extension );
+
+        return $this->kernel->locateResource($fileToLocate);
+    }
+
+    /**
+     * Get The Bundle Name of an entity class
+     * @param String an entity namespace
+     * @return the Bundle name of the entity
+     */
+    private function getBundleNameForClass($rootEntityName) {
+        $bundles = $this->kernel->getBundles();
+        $bundleName = null;
+
+        foreach($bundles as $type => $bundle){
+            $className = get_class($bundle);
+
+            $entityClass = substr($rootEntityName,0,strpos($rootEntityName,'\\Entity\\'));
+
+            if(strpos($className,$entityClass) !== FALSE){
+                $bundleName = $type;
+            }
+        }
+
+        if (null === $bundleName) {
+            throw new \Exception(sprintf("Bundle was not found for entity %s, maybe you should declare it in AppKernel", $rootEntityName));
+        }
+
+        return $bundleName;
     }
 }
