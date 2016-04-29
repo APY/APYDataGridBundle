1503601c062bc690be2fae82ece6d193bfe8a9bf
Eligijus Vitkauskas <eligijus.vitkauskas@nfq.lt>
2016-04-29 12:23:14 +0300

    Load console commands only if SensioGeneratorBundle is enabled. Fixes #858

--- APYDataGridBundle.php
+++ APYDataGridBundle.php
@@ -14,6 +14,8 @@ namespace APY\DataGridBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Console\Application as SymfonyConsoleApplication;
 use APY\DataGridBundle\DependencyInjection\Compiler\GridExtensionPass;
 
 class APYDataGridBundle extends Bundle
@@ -23,5 +25,36 @@ class APYDataGridBundle extends Bundle
         parent::build($container);
 
         $container->addCompilerPass(new GridExtensionPass());
+    }
+
+    /**
+     * Register bundle commands.
+     *
+     * Register commands from Command directory only if SensioGeneratorBundle
+     * is registered in Kernel.
+     *
+     * Whenever Application if not an instance of SymfonyConsoleApplication we
+     * just stick to the default behavior.
+     *
+     * @param Application $application
+     */
+    public function registerCommands(Application $application)
+    {
+        if (!$application instanceof SymfonyConsoleApplication) {
+            parent::registerCommands($application);
+
+            return;
+        }
+
+        try {
+            $application->getKernel()->getBundle('SensioGeneratorBundle');
+            parent::registerCommands($application);
+        } catch (\InvalidArgumentException $e) {
+            // $kernel->getBundle() throws \InvalidArgumentException whenever
+            // bundle specified is not registered. We are only providing
+            // commands based on SensioGenerator functionality, so whenever
+            // there are not SensioGeneratorBundle available we MUST not
+            // register our commands aswell.
+        }
     }
 }
